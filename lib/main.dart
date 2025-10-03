7883213e2f13961f132b871aca2f82eaedf385ba
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-10-03 10:42:35 +0700

    Dasar Widget Container

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Column & Row Example')),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Atas"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.star, color: Colors.red, size: 40),
-                    Icon(Icons.star, color: Colors.red, size: 40),
-                    Icon(Icons.star, color: Colors.red, size: 40),
-                  ],
-                ),
-                const Text("Bawah"),
-              ],
-            ),
+          title: const Text('Container Widget'),),
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.amber,
+            child: const Center(
+              child: Text("Halo Flutter"),
             ),
           ),
-    );
+      ),
+      );
   }
 }
+//           Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 const Text("Atas"),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: const [
+//                     Icon(Icons.star, color: Colors.red, size: 40),
+//                     Icon(Icons.star, color: Colors.red, size: 40),
+//                     Icon(Icons.star, color: Colors.red, size: 40),
+//                   ],
+//                 ),
+//                 const Text("Bawah"),
+//               ],
+//             ),
+//             ),
+//           ),
+//     );
+//   }
+// }
